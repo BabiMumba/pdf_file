157a88b2528f1ca5c552402a094585d599cdd4be
Babi Mumba <babimumba243@gmail.com>
2022-06-16 20:42:34 +0200

    new file

--- app/src/main/java/com/babistone/monp/MainActivity.kt
+++ app/src/main/java/com/babistone/monp/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.babistone.monp
 
 import android.app.DownloadManager
+import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    private var tvFileName: TextView? = null
+    private var btnDownload: Button? = null
+    private  var btnView:android.widget.Button? = null
+
+    private val filepath = "http://africau.edu/images/default/sample.pdf"
+    private var url: URL? = null
+    private var fileName: String? = null
+
     private lateinit var txtv:EditText
     private lateinit var valider:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        setListeners()
 
         txtv = findViewById(R.id.lien)
         valider = findViewById(R.id.tlcharger)
@@ -29,6 +48,51 @@ class MainActivity : AppCompatActivity() {
         }
         effacer.setOnClickListener {
             lien.setText("")
+        }
+
+
+
+    }
+
+
+
+    private fun initViews() {
+        tvFileName = findViewById(R.id.tvUrl)
+        btnDownload = findViewById(R.id.btnDownload)
+        btnView = findViewById(R.id.btnView)
+
+
+        try {
+            url = URL(filepath)
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        }
+
+        fileName = url!!.path
+        fileName = fileName.substring(fileName.lastIndexOf('/') + 1)
+        tvFileName.setText(fileName)
+
+    }
+    private fun setListeners() {
+        btnDownload!!.setOnClickListener { v: View? ->
+            val request = DownloadManager.Request(Uri.parse(url.toString() + ""))
+            request.setTitle(fileName)
+            request.setMimeType("applcation/pdf")
+            request.allowScanningByMediaScanner()
+            request.setAllowedOverMetered(true)
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+        }
+
+        btnView!!.setOnClickListener { v: View? ->
+            val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/" + fileName)
+            val uri = FileProvider.getUriForFile(this, "com.example.firstproject" + ".provider", file)
+            val i = Intent(Intent.ACTION_VIEW)
+            i.setDataAndType(uri, "application/pdf")
+            i.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_GRANT_READ_URI_PERMISSION
+            startActivity(i)
         }
 
     }
