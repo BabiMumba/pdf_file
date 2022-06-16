ddcc9c80cf658453700f6f6cd21ea732e07dee6a
Babi Mumba <babimumba243@gmail.com>
2022-06-16 20:07:43 +0200

    new file

--- app/src/main/java/com/babistone/monp/LivrepdfActivity.java
+++ app/src/main/java/com/babistone/monp/LivrepdfActivity.java
@@ -3,13 +3,25 @@ package com.babistone.monp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.URL;
 
 public class LivrepdfActivity extends AppCompatActivity {
+    private TextView tvFileName;
+    private Button btnDownload, btnView;
+
+    private String filepath = "http://africau.edu/images/default/sample.pdf";
+    private URL url = null;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_livrepdf);
+        setContentView(R.layout.activity_main2);
 
+        initViews();
+        setListeners();
     }
 }
